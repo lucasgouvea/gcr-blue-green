55314bb2efe3ed7dacf0ce18939aa253b8712cff
Lucas Gouvea <lucasgouvea93@gmail.com>
2023-09-20 22:16:16 -0300

    feat: broken app

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ app.get('/health-check', (req, res) => {
     }
 );
 
-app.listen(3000, () => {
+
+console.log("broken app")
+setTimeout(() => {
+  // This function will be executed after a very long time (effectively, indefinitely).
+}, 999999999);
+
+/* app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
-    }   
-);
+    }
+); */
 
